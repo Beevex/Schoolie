49a8a2995d04149a5c5e638995af8145f6dad43d
Beevex <bioudsamir5@gmail.com>
2020-10-02 20:08:00 +0100

    commitment

--- src/com/beevex/mathops.java
+++ src/com/beevex/mathops.java
@@ -2,7 +2,6 @@ package com.beevex;
 
 public class mathops {
     public static int add(int no1, int no2) {
-        int response = no1 + no2;
-        return response;
+        return no1 + no2;
     }
 }
--- src/com/beevex/menu.java
+++ src/com/beevex/menu.java
@@ -1,12 +1,12 @@
 package com.beevex;
-import java.util.Scanner;
+
 public class menu {
-    static Scanner scanner = new Scanner(System.in);
     public static void menuChoices(String whatToDo){
         switch(whatToDo){
             case("maths"):
                 mathio.mathematics();
                 break;
+                //more cases will be created for different menu options
         }
     }
 }
